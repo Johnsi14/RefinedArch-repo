5be7139338cc090f279223047b0bc803dce8abbf
Jonas Ziegler <zieglerjonas14@gmail.com>
2024-03-21 21:33:03 +0100

    Made the update Script actually work

--- update_db.sh
+++ update_db.sh
@@ -32,23 +32,12 @@ echo "###################################"
 ## -R: remove old package files when updating their entry
 repo-add -s -n -R refined-repo.db.tar.gz */*.pkg.tar.zst
 
-
-#Check if the symlinks should be removed because the repository may be on GitLab
-if (( "$#" == 1 )) 
-then
-   if [ "$1" = "-r" ] 
-    then
-    echo "something"
-    fi 
-exit 1
-fi
-
-
 echo "#######################################"
 echo "Packages in the repo have been updated!"
 echo "#######################################"
 
-removing_symlinks() {
+
+remove_symlinks() {
 # Removing the symlinks because GitLab can't handle them.
 rm refined-repo.db
 rm refined-repo.db.sig
@@ -65,3 +54,12 @@ echo "#######################################"
 echo "Symlinks have been removed"
 echo "#######################################"
 }
+
+#Check if the symlinks should be removed because the repository may be on GitLab
+if (( "$#" == 1 )) 
+then
+   if [ "$1" = "-r" ] 
+    then
+    remove_symlinks
+    fi 
+fi
